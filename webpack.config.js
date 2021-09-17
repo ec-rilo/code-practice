f9b12ecb5ad508bdec83e589f06429b263a80d7d
Edgar Carrillo <ecarrillo046@gmail.com>
2021-09-17 12:37:09 -0700

    Add starter files and folders

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Practice Program Template',
+      template: './src/index.html'
+    })
+  ],
+  devtool: 'inline-source-map',
+  
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
