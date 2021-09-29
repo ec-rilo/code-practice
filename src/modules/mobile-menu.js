e538a79f36c9121ba6bbf4e45b3202c85555cafd
Edgar <ecarrillo046@gmail.com>
2021-09-28 18:52:34 -0700

    Add closeHamBtn logic and openMenu logic

--- src/modules/mobile-menu.js
+++ src/modules/mobile-menu.js
@@ -1,7 +1,48 @@
 "use strict";
 
 let initMenuAnimation = (() => {
-  function delay();
+  let notHamBtn = document.querySelector(".not-ham-btn");
 
+  function closeNotHamBtn() {
+    const notHamBtnTop = notHamBtn.getElementsByTagName("div")[0];
+    notHamBtnTop.classList.add("slide-left-first");
 
+    const notHamBtnBottom = notHamBtn.getElementsByTagName("div")[1];
+    notHamBtnBottom.classList.add("slide-left-second");
+
+    setTimeout(() => {
+      notHamBtnTop.style.display = "none";
+      notHamBtnBottom.style.display = "none";
+    }, 2000);
+  }
+
+  function openCircles() {
+    const circlesContainer = document.querySelector(".ham-menu-container");
+    circlesContainer.style.display = "block";
+
+    const smallCircle = circlesContainer.getElementsByTagName("div")[0];
+    const mediumCircle = circlesContainer.getElementsByTagName("div")[1];
+    const largeCircle = circlesContainer.getElementsByTagName("div")[2];
+
+    const circlesArr = [smallCircle, mediumCircle, largeCircle];
+    circlesArr.forEach((circle) => {
+      circle.classList.add("open-circle");
+    });
+  }
+
+  function openCloseBtn() {
+    // Make it able to close the menu options with a fade out from top animation for each
+    // Close the circles
+    // Make the ham button appear
+  }
+
+  notHamBtn.addEventListener("click", () => {
+    closeNotHamBtn();
+    openCircles();
+    // Close the not ham btn and set it to invisible.
+    // open the circles
+    // Open the close button
+    // Open the menu items
+    // For each menu item add a fade in from bottom animation for each
+  });
 })();
